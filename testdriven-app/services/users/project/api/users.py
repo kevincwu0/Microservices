e70985baf7ad26bc3973a5b3afbf4f8057fdbd0e
kevincwu0 <kevincwu0@gmail.com>
2019-07-07 18:49:06 -0400

    Add jinja templates

--- testdriven-app/services/users/project/api/users.py
+++ testdriven-app/services/users/project/api/users.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 from flask_restful import Resource, Api
 
 from project import db
 from project.api.models import User
 from sqlalchemy import exc
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 api = Api(users_blueprint)
 
 class UsersPing(Resource):
@@ -81,3 +81,13 @@ class Users(Resource):
 api.add_resource(UsersPing, '/users/ping')
 api.add_resource(UsersList, '/users')
 api.add_resource(Users, '/users/<user_id>')
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.all()
+    return render_template('index.html', users=users)
