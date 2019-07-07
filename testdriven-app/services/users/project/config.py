95ddf803d6722a6da794024f776e18119c83aaf5
kevincwu0 <kevincwu0@gmail.com>
2019-07-07 16:04:09 -0400

    Add postgres service

--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -1,12 +1,15 @@
+import os
 
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig:
-    pass
+    SQLACHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class TestingConfig:
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
